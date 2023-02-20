68b4563776f7d5680a036c34098b3272081b6755
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-20 15:06:38 +0100

    Add header with vue + html & css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,34 @@
 
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      element: [
+        {
+          name: "Home",
+          url: "",
+        },
+        {
+          name: "Services",
+          url: "",
+        },
+        {
+          name: "About",
+          url: "",
+        },
+        {
+          name: "Videos",
+          url: "",
+        },
+        {
+          name: "Blog",
+          url: "",
+        },
+        {
+          name: "Store",
+          url: "",
+        },
+      ],
+    };
   },
 });
 
