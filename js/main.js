fb1a009084d761a895b5021c0380b650fbd15f49
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-21 23:39:34 +0100

    Add footer

--- js/main.js
+++ js/main.js
@@ -29,6 +29,29 @@ const app = Vue.createApp({
           url: "",
         },
       ],
+
+      footer: [
+        {
+          name: "The best protein shake",
+          url: "",
+        },
+        {
+          name: "Ultimate cardio workout",
+          url: "",
+        },
+        {
+          name: "New juices available now",
+          url: "",
+        },
+        {
+          name: "Tips to find training partners",
+          url: "",
+        },
+        {
+          name: "20 best healthy recipes",
+          url: "",
+        },
+      ],
     };
   },
 });
